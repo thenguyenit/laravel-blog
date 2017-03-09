bc0a5f69a15309f22a1039e531dbf0bd6b88dd53
Nguyen Nguyen <n.the@rainbow-software.vn>
2017-03-09 17:24:11 +0700

    Improve load css and js

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -19,14 +19,14 @@
 <!-- App js -->
 <script async src="{{asset('js/app.js')}}"></script>
 
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
 <noscript id="deferred-styles">
-    {{--<link rel="stylesheet" type="text/css" href="small.css"/>--}}
-    <!-- Bootstrap Core CSS -->
-    <link href="{{asset('components/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="{{asset('css/my-style.css')}}" rel="stylesheet">
-
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
