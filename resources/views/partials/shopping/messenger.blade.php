a2d0229cc972b3fabce677c7133c3a3e7592464f
ntnguyen <ntnguyen@fossil.com>
2018-11-27 22:05:03 -0600

    Adding T-Rex go shopping

--- resources/views/partials/shopping/messenger.blade.php
+++ resources/views/partials/shopping/messenger.blade.php
@@ -1,6 +1,15 @@
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId            : '160529061351612',
+          autoLogAppEvents : true,
+          xfbml            : true,
+          version          : 'v2.11'
+        });
+    };
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
@@ -16,3 +25,12 @@
      logged_in_greeting="Hi! Chat với chúng tôi nhé!"
      logged_out_greeting="Hi! Chat với chúng tôi nhé!">
 </div>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.2&appId=213149345514026&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
